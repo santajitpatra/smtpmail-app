6305ef01ad114ce994350f31b1de2450f76f8aa0
Santajit Patra <121734312+santajitpatra@users.noreply.github.com>
2024-03-18 23:31:35 +0530

    refactor: SMTP server - env variables, logging, dynamic port
    refactor: SMTP server - env variables, logging, dynamic port
    
    This commit refactors the SMTP server code to:
    
    * Utilize environment variables for port configuration (process.env.PORT).
    * Add console logging for connection events, sender/recipient information.
    * Enable dynamic port listening based on the environment variable.

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const SMTPServer = require("smtp-server").SMTPServer;
 
+const PORT = process.env.PORT
 const server = new SMTPServer({
     allowInsecureAuth: true,
     authOptional: true,
   onConnect(session, callback) {
+    console.log("Connected to %s", session.id);
     callback(); // Accept the connection
   },
   onMailFrom(address, session, callback) {
+    console.log("Mail from %s", address.address, session.id);
     callback(); // Accept the address
   },
   onRcptTo(address, session, callback) {
+    console.log("Receiving mail for %s", address.address, session.id);
     callback(); // Accept the address
   },
   onData(stream, session, callback) {
@@ -19,11 +23,10 @@ const server = new SMTPServer({
     stream.on("end", () => {
       callback(); // Accept the message
     });
-    // stream.resume();
   }
 
 });
 
-server.listen(25, "0.0.0.0", () => {
+server.listen(PORT, () => {
   console.log("SMTP server listening on port 25");
 });
